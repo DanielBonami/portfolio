767c6394a45e68db99bdef297e71fbe890eaf991
DanielBonami <bonamidaniel4@gmail.com>
2025-08-07 14:28:55 -0500

    Add Pulse Dashboard project to mobile apps section

--- assets/assets.js
+++ assets/assets.js
@@ -101,7 +101,7 @@ export const workData = [
 
 export const serviceData = [
     { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: 'https://real-estate-ten-delta.vercel.app/' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: 'https://issue-tracker-azure-five.vercel.app/' },
+    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: 'https://pulseboard1.vercel.app/dashboard' },
     { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: 'https://backgroundgenerator-phi.vercel.app/' },
     { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: 'https://portfolio-five-psi-47.vercel.app/' },
 ]
