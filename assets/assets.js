6fcc6517c1f6d2c5d92d0544e0c2274de2952668
DanielBonami <bonamidaniel4@gmail.com>
2025-08-14 16:03:17 -0500

    changed the  languages description in my about me  to html5,css3,javascript, react js, next js.

--- assets/assets.js
+++ assets/assets.js
@@ -107,7 +107,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML5, CSS3, JavaScript, React Js, Next Js' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Complete Web Developer from Zero to Mastery' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 3 projects' }
 ];
